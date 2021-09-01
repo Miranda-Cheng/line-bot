3eb5e144bc9299b7ffbe4dddbbab08967e311203
Miranda Cheng <t05230520@gmail.com>
2021-09-01 15:50:59 +0800

    try

--- app.py
+++ app.py
@@ -38,10 +38,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '早安'
+    reply = '對不起！我不知道你在說什麼'
+
+    if msg == 'hi':
+        reply = 'hello'
+    elif msg == '你吃飯了嗎':
+        reply = '機器人不用吃飯'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=reply))
 
 
 if __name__ == "__main__":
